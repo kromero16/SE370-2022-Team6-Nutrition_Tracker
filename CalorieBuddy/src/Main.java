12ea9b49c8d99917669ebe5eb3134a861148beb2
kromero16 <kevkvn13@live.com>
2022-11-04 11:39:03 -0700

    test db connection

--- CalorieBuddy/src/Main.java
+++ CalorieBuddy/src/Main.java
@@ -1,6 +1,33 @@
-
+import java.sql.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("CHANGE MADE");
+        System.out.println("test");
+
+        //testing database  connection
+        String databaseURL = "jdbc:mysql://localhost";
+        String user = "root";
+        String password = "admin";
+        Connection conn = null;
+        try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(databaseURL, user, password);
+            if (conn != null) {
+                System.out.println("Connected to the database");
+            }
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Could not find database driver class");
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            System.out.println("An error occurred. Maybe user/password is invalid");
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
