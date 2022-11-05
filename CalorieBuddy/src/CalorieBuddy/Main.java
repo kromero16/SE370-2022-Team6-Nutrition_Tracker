a7c59f1592f5eff8856acbdf5cb4e4f7e54b083b
kromero16 <kevkvn13@live.com>
2022-11-04 23:06:00 -0700

    successfully tested JDBC to AWS RDS

--- CalorieBuddy/src/CalorieBuddy/Main.java
+++ CalorieBuddy/src/CalorieBuddy/Main.java
@@ -1,10 +1,38 @@
 package CalorieBuddy;
 
+import java.sql.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Changed IDE!");
 
+		//testing database  connection
+        String databaseURL = "jdbc:mysql://db1.cojsra38kk1x.us-west-2.rds.amazonaws.com";
+        String user = "root";
+        String password = "csusmpassword";
+        Connection conn = null;
+        try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(databaseURL, user, password);
+            if (conn != null) {
+                System.out.println("Connected to the database");
+            }
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Could not find database driver class");
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            System.out.println("An error occurred. Maybe user/password is invalid");
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
 	}
 
 }
