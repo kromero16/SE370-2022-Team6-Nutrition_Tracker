ecc4e012f0d7a26dd50044883c619b0f55990f14
kromero16 <kevkvn13@live.com>
2022-11-07 15:43:35 -0800

    test

--- CalorieBuddy/src/CalorieBuddy/Main.java
+++ CalorieBuddy/src/CalorieBuddy/Main.java
@@ -1,43 +1,20 @@
 package CalorieBuddy;
 
 import java.sql.*;
+import java.net.URLEncoder;
+import com.mashape.unirest.http.HttpResponse;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		//test output
-		System.out.println("Changed IDE!");
+		System.out.println("Testing API");
 		
 		//create registration form object
-		new RegistrationForm();
+		//new RegistrationForm();
 
-	/*	//testing database  connection
-        String databaseURL = "jdbc:mysql://database-1.cojsra38kk1x.us-west-2.rds.amazonaws.com:3306/CalorieBuddy";
-        String user = "root";
-        String password = "iVAY65ivErrpcXTr6J9g";
-        Connection conn = null;
-        try {
-           Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(databaseURL, user, password);
-            if (conn != null) {
-                System.out.println("Connected to the database");
-            }
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Could not find database driver class");
-            ex.printStackTrace();
-        } catch (SQLException ex) {
-            System.out.println("An error occurred. Maybe user/password is invalid");
-            ex.printStackTrace();
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }*/
+
 	}
 
 }
